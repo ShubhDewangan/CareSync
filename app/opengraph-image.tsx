6475d019990055230dcc7f41d8dc18cd12643373
Shubh Dewangan <shubhdwngn24@gmail.com>
2026-05-24 17:32:40 +0530

    optimizing og/4

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import { ImageResponse } from "next/og";
 import { readFile } from "fs/promises";
 import { join } from "path";
